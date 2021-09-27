3e04bed0cd2d6ba1ddfdd3847ef8b5e66e1fcd32
Veronika Ranta <veronika@ranta.com>
2021-09-27 11:37:21 +0200

    Fixade navbar

--- dom.js
+++ dom.js
@@ -3,16 +3,15 @@
 $(document).ready(function() {
 
     $('#hamburger-click').click(function() {
-
         $('div#navbar-home').show(500);
         $('#hamburger-click').hide();
         $('#cross').show(500);
-
     });
 
     $('#cross').click(function() {
         $('div#navbar-home').hide(500);
         $('#hamburger-click').show(500);
+        document.getElementById("hamburger-click").setAttribute("style", "display: block");
         $('#cross').hide();
     });
 
